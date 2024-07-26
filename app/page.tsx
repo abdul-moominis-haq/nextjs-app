aa9c97979c6918146d6d53044d35f431b8f0016b
IS-HAQ ABDUL-MOOMIN <114371157+abdul-moominis-haq@users.noreply.github.com>
2024-07-26 07:32:46 +0000

    Shadcn button for click

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from "@/components/ui/button"
 
 export default function componentName() {
   return (
     <>
     <h1>Hello World</h1>
+    <div>
+      <Button>Click me</Button>
+    </div>
     </>
   );
 }
